c4ed88dde6ab212b8c17a9690b89b7aabff51888
sush911 <sshres381@gmail.com>
2025-06-12 23:18:49 +0545

    little changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+//can hit api
 router.post(
   '/register',
   [
     body('name').notEmpty().withMessage('Name is required'),
-    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('email').isEmail().withMessage('Please enter your valid email'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ],
   authController.register
