61f71b49f676c86280d5e16d838c1d9a7ee4dc48
sush911 <sshres381@gmail.com>
2025-06-21 13:33:49 +0545

    routes rescue

---
+++ backend/routes/rescue.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const rescueController = require('../controllers/rescueController');
+
+router.get('/', rescueController.getAllRescues);
+router.get('/:id', rescueController.getRescueById);
+router.post('/', rescueController.createRescue);
+router.put('/:id', rescueController.updateRescue);
+router.delete('/:id', rescueController.deleteRescue);
+
+module.exports = router;
